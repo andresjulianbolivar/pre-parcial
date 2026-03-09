e45b716844a5cd08cf514f25557c8cbb5b23902c
andres bolivar <a.bolivarc@uniandes.edu.co>
2026-03-09 01:08:06 -0500

    fix: Actualización de estilo de las listas.

--- src/app/crear/page.tsx
+++ src/app/crear/page.tsx
@@ -3,7 +3,9 @@ import Lista from "@/components/Lista";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="mb-4">
       <h1 className="text-4xl font-bold">Página Crear Autor</h1>
+      </div>
       <Lista />
     </main>
   );
--- src/components/Lista.tsx
+++ src/components/Lista.tsx
@@ -1,12 +1,49 @@
 "use client";
 import { useAutores } from "./AutoresContext";
+import Image from "next/image";
 
 export default function Lista(){
     const {autores, actualizarAutores} = useAutores();
 
     const listItems = autores.map(autor =>
-        <li key={autor.name}>{autor.name}</li>
+        <tr className="bg-neutral-primary border-b border-default" key={autor.name}>
+            <th scope="row" className="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                {autor.name}
+            </th>
+            <td className="px-6 py-4">
+                {autor.birthDate}
+            </td>
+            <td className="px-6 py-4">
+                {autor.description}
+            </td>
+            <td>
+                <Image src={autor.image} alt={autor.name} width={50} height={50} />
+            </td>
+            <td>
+                <div className="d-flex flex-wrap gap-2 justify-content-end">
+                    <button disabled={false} type="button" className="px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-600" title="Editar">Editar</button>
+                    <button disabled={false} type="button" className="px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600" title="Eliminar">Eliminar</button>
+                </div>
+            </td>
+        </tr>
     );
 
-    return <ol>{listItems}</ol>;
+    return (
+        <div className="relative overflow-x-auto bg-neutral-primary-soft shadow-xs rounded-base border border-default">
+            <table className="w-full text-sm text-left rtl:text-right text-body">
+                <thead className="text-sm text-body bg-neutral-secondary-soft border-b rounded-base border-default">
+                    <tr>
+                        <th scope="col" className="px-6 py-3 font-medium">Nombre</th>
+                        <th scope="col" className="px-6 py-3 font-medium">Fecha de nacimiento</th>
+                        <th scope="col" className="px-6 py-3 font-medium">Descripción</th>
+                        <th scope="col" className="px-6 py-3 font-medium">Imagen</th>
+                        <th scope="col" className="px-6 py-3 font-medium">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listItems}
+                </tbody>
+            </table>
+        </div>
+    );
 }
